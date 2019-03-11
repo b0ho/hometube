06aead16a2f945090a834cb2eb632e5705e4e772
b0ho <ybg4828@gmail.com>
2019-03-11 13:46:38 +0900

    new_init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as THREE from 'three';
+import words from 'random-words';
+import Cube from './Cube';
 
 class App extends Component {
   render() {
+    console.log(words(5));
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>hi</div>,
+      <Cube></Cube>,
+      <div>hello</div>
     );
   }
 }
